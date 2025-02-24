34240b39df6249fa5ce203c801c82b6b8492a6aa
alejandra marino <alejamarino123@gmail.com>
2025-02-23 23:44:42 -0500

    create client markup page

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -1,10 +1,17 @@
 import ClientList from "@/components/ClientList";
+import Header from "@/components/Header";
+import ClientFilters from "@/components/ClientFilters";
+
 export default async function AgentsPage() {
   return (
-    <div>
-      <h1>Lista de Clientes</h1>
-      <div className="grid grid-cols-3 gap-4">
-        <ClientList />
+    <div className=" flex flex-row  bg-blue-100 text-white min-h-screen relative">
+      <Header />
+      <div className="flex-1 flex flex-col p-4 h-screen overflow-y-auto">
+        <div className="flex flex-col items-center h-screen bg-blue-100 text-white">
+          <h1 className="text-7xl text-center text-black py-10">Clientes</h1>
+          <ClientFilters />
+          <ClientList />
+        </div>
       </div>
     </div>
   );
