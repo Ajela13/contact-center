c88144cea0d397e80eba3630459a4e5f39bbbe3d
alejandra marino <alejamarino123@gmail.com>
2025-02-23 23:44:36 -0500

    create agent markup page

--- src/app/agents/page.jsx
+++ src/app/agents/page.jsx
@@ -1,16 +1,17 @@
-import { fetchAgents } from "@/services/api";
-import Filters from "@/components/Filters";
+import AgentFilters from "@/components/AgentFilters";
 import AgentsList from "@/components/AgentsList";
+import Header from "@/components/Header";
 
 export default async function AgentsPage() {
-  const agents = await fetchAgents();
-
   return (
-    <div>
-      <h1>Lista de Agentes</h1>
-      <Filters />
-      <div className="grid grid-cols-3 gap-4">
-        <AgentsList />
+    <div className=" flex flex-row  bg-blue-100 text-white min-h-screen relative">
+      <Header />
+      <div className="flex-1 flex flex-col p-4 h-screen overflow-y-auto">
+        <div className="flex flex-col items-center h-screen bg-blue-100 text-white">
+          <h1 className="text-7xl text-center text-blue-950 py-10">Agentes</h1>
+          <AgentFilters />
+          <AgentsList />
+        </div>
       </div>
     </div>
   );
