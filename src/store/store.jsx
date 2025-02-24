c8a15b108468062d2dc7a488e17708c3864fb886
alejandra marino <alejamarino123@gmail.com>
2025-02-24 04:23:30 -0500

    add stateFilter

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,8 +1,10 @@
 import { create } from "zustand";
 
 export const useStore = create((set) => ({
+  stateFilter: "",
   agents: [],
   clients: [],
   setAgents: (agents) => set({ agents }),
   setClients: (clients) => set({ clients }),
+  setStateFilter: (stateFilter) => set({ stateFilter }),
 }));
