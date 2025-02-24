8671ca3e2434528d089d01c9a85313046bee590f
alejandra marino <alejamarino123@gmail.com>
2025-02-24 04:23:12 -0500

    feat: add dynamic api for queryparams

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,21 @@
 const API_URL = "http://localhost:5248";
 
-export const fetchAgents = async () => {
-  const res = await fetch(`${API_URL}/agents`);
+export const fetchAgents = async (state) => {
+  const url = state ? `${API_URL}/agents?state=${state}` : `${API_URL}/agents`;
+
+  const res = await fetch(url);
   if (!res.ok) throw new Error("Error fetching agents");
+
   return res.json();
 };
 
-export const fetchClients = async () => {
-  const res = await fetch(`${API_URL}/clients`);
+export const fetchClients = async (maxWaitTime) => {
+  const url = maxWaitTime
+    ? `${API_URL}/clients?maxWaitTime=${maxWaitTime}`
+    : `${API_URL}/clients`;
+
+  const res = await fetch(url);
   if (!res.ok) throw new Error("Error fetching clients");
+
   return res.json();
 };
